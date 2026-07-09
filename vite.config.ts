42bce064657b318e93c071e4370912a44d04851f
Karan_shekhawat <thestenosingh@gmail.com>
2026-07-09 21:06:53 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/MathTracker/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
